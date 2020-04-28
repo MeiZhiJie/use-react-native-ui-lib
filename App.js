172f9bc8abefaa3831b6ec1d1554d9faeb756f70
MeiZhiJie <meizj@hotmail.com>
2020-04-28 09:53:09 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+//import 'react-native-gesture-handler';
+import React from 'react';
+import _ from 'lodash';
+//import {screens} from 'unicorn-demo-app';
+import {screens} from './src';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import MainScreen from './MainScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={MainScreen}/>
+        {_.map(screens, (screen, key) => {
+          return <Stack.Screen key={key} name={key.replace('Screen', '')} component={screen}/>;
+        })}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
